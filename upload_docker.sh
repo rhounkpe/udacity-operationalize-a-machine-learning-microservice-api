2f24798fd3d8c3cdd1c0a5fc6403e91c78798ac8
Rufin Hounkpe <rhounkpe@gmail.com>
2020-07-25 00:13:20 +0200

    Uploading the Docker image

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,14 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+dockerpath=udacity-cloud-devops-project-rufin-hounkpe
 
 # Step 2:  
 # Authenticate & tag
 echo "Docker ID and Image: $dockerpath"
+docker login --username rhounkpe
+docker tag udacity-cloud-devops-project-rufin-hounkpe rhounkpe/udacity-cloud-devops-project-rufin-hounkpe:v1
 
 # Step 3:
 # Push image to a docker repository
+docker push rhounkpe/udacity-cloud-devops-project-rufin-hounkpe:v1
