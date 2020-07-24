fc3fe270f1f7ea419736e593bdfcb93f91826aca
Rufin Hounkpe <rhounkpe@gmail.com>
2020-07-25 01:05:11 +0200

    Deploying with Kubernetes

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,14 +5,17 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
+dockerpath=rhounkpe/udacity-cloud-devops-project-rufin-hounkpe
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run dacity-cloud-devops-project-rufin-hounkpe --image=rhounkpe/udacity-cloud-devops-project-rufin-hounkpe --port=80
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
+kubectl expose deployment udacity-cloud-devops-project-rufin-hounkpe --type=LoadBalancer --port=8000 --target-port=80
 
