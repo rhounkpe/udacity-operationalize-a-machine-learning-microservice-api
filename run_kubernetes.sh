f7676d52779df7b2d1d83442d8f32d1933da1e1c
Rufin Hounkpe <rhounkpe@gmail.com>
2020-07-25 01:54:29 +0200

    Refactoring

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -10,7 +10,10 @@ dockerpath="rhounkpe/udacity-cloud-devops-project-rufin-hounkpe"
 # Step 2
 # Run the Docker Hub container with kubernetes
 # kubectl create deployment dacity-cloud-devops-project-rufin-hounkpe --image=$dockerpath
-kubectl run "dacity-cloud-devops-project-rufin-hounkpe" --generator=run-pod/v1 --image=$dockerpath --port=80 --labels app="dacity-cloud-devops-project-rufin-hounkpe"
+kubectl run udacity-cloud-devops-project-rufin-hounkpe\
+    --generator=run-pod/v1\
+    --image=$dockerpath\
+    --port=80 --labels app=udacity-cloud-devops-project-rufin-hounkpe
 
 # Step 3:
 # List kubernetes pods
@@ -18,6 +21,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward dacity-cloud-devops-project-rufin-hounkpe 8000:80
-# kubectl expose deployment udacity-cloud-devops-project-rufin-hounkpe --type="Nodeport" --target-port=80
-# kubectl port-forward deployment/udacity-cloud-devops-project-rufin-hounkpe 8000:80
+kubectl port-forward udacity-cloud-devops-project-rufin-hounkpe 8000:80
