9d9576abb431672f38c31a88719879079464a5ca
Rufin Hounkpe <rhounkpe@gmail.com>
2020-08-02 20:11:36 +0200

    Adding kubernetes_out.txt

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -5,15 +5,14 @@
 # Step 1:
 # This is your Docker ID/path
 # dockerpath=<>
-dockerpath="rhounkpe/udacity-cloud-devops-project-rufin-hounkpe"
+dockerpath="rhounkpe/udacity-cloud-devops-project-rufin-hounkpe:v1"
 
 # Step 2
 # Run the Docker Hub container with kubernetes
 # kubectl create deployment dacity-cloud-devops-project-rufin-hounkpe --image=$dockerpath
-kubectl run udacity-cloud-devops-project-rufin-hounkpe\
-    --generator=run-pod/v1\
+kubectl run hounkpedemo\
     --image=$dockerpath\
-    --port=80 --labels app=udacity-cloud-devops-project-rufin-hounkpe
+    --port=80 --labels="app=hounkpedemo"
 
 # Step 3:
 # List kubernetes pods
@@ -21,4 +20,4 @@ kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-kubectl port-forward udacity-cloud-devops-project-rufin-hounkpe 8000:80
+kubectl port-forward hounkpedemo 8000:80
